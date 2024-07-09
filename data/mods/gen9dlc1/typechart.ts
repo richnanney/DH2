8f435ac0b5b0f51980b82e9a20d87f2c3006d385
richnanney <richardpnanney@gmail.com>
2024-07-08 21:35:01 -0600

    removed all old mods

--- data/mods/gen9dlc1/typechart.ts
+++
@@ -1,6 +0,0 @@
-export const TypeChart: {[k: string]: ModdedTypeData | null} = {
-	stellar: {
-		inherit: true,
-		isNonstandard: 'Future',
-	},
-};
--- data/mods/kitchen/formats-data.ts
+++
@@ -1,7 +0,0 @@
-export const FormatsData: {[k: string]: SpeciesFormatsData} = {
-	
-	athleetah: {
-		tier: "OU",
-		doublesTier: "OU",
-	},
-};
