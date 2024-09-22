7226f24b2d1e2f95409db4bea8275eb3f5b5cbee
richnanney <richardpnanney@gmail.com>
2024-09-21 18:45:16 -0600

    Update conditions.ts

--- data/mods/unbound/conditions.ts
+++ data/mods/unbound/conditions.ts
@@ -19,45 +19,20 @@ export const Conditions: {[k: string]: ModdedConditionData} = {
 	frz: {
         name: 'frz',
         effectType: 'Status',
-        onStart(target, source, sourceEffect) {
-            if (sourceEffect && sourceEffect.effectType === 'Ability') {
-                this.add('-status', target, 'frz', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
-            } else {
-                this.add('-status', target, 'frz');
+		onStart(target, source, sourceEffect) {
+            this.add('-status', target, 'frz');
+		},
+        onBasePower(basePower, attacker, defender, move) {
+			if (move.category === 'Special') {
+				return this.chainModify(.5);
             }
-            if (target.species.name === 'Shaymin-Sky' && target.baseSpecies.baseSpecies === 'Shaymin') {
-                target.formeChange('Shaymin', this.effect, true);
-            }
-        },
-        onBeforeMovePriority: 10,
-        onBeforeMove(pokemon, target, move) {
-            if (move.flags['defrost']) return;
-            if (this.randomChance(1, 5)) {
-                pokemon.cureStatus();
-                return;
-            }
-            this.add('cant', pokemon, 'frz');
-            return false;
-        },
-        onModifyMove(move, pokemon) {
-            if (move.flags['defrost']) {
-                this.add('-curestatus', pokemon, 'frz', '[from] move: ' + move);
-                pokemon.clearStatus();
-            }
-        },
-        onAfterMoveSecondary(target, source, move) {
-            if (move.thawsTarget) {
-                target.cureStatus();
-            }
-        },
-        onDamagingHit(damage, target, source, move) {
-            if (move.type === 'Fire' && move.category !== 'Status') {
-                target.cureStatus();
-            }
-        },
-    },
+		},
+		onResidualOrder: 10,
+		onResidual(pokemon) {
+			this.damage(pokemon.baseMaxhp / 8);
+		},
 
-	
+	},
 	/*
 	Freeze -> Frostbite (halves special attack)
 	Frostbite does 1/8 damage (like burn)
