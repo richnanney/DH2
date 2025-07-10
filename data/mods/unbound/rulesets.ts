9d1cbb3006d1fa7a8db3444059ed35618d25b267
Richard Nanney <79386248+richnanney@users.noreply.github.com>
2025-07-09 18:21:09 -0600

    Update rulesets.ts

--- data/mods/unbound/rulesets.ts
+++ data/mods/unbound/rulesets.ts
@@ -1,12 +1,13 @@
 export const Rulesets: {[k: string]: ModdedFormatData} = {
 	waterGym: {
-		effectType: 'ValidatorRule',
+		effectType: 'Rule',
 		name: 'Water Gym',
 		desc: 'Battle effects for a water-type gym. Primarily includes perma-rain.',
 		onBegin() {
-			this.add('-weather', 'Rain Dance');
-			this.field.weather = 'raindance' as ID;
-			this.field.weatherState = { id: 'raindance'};
+			this.field.setWeather('raindance');
+			// this.add('-weather', 'Rain Dance');
+			// this.field.weather = 'raindance' as ID;
+			// this.field.weatherState = { id: 'raindance'};
 		}
 	}
 }
