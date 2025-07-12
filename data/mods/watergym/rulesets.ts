1bb4e4f137b94ad3bdc12c0368553f7fe32334c7
Richard Nanney <79386248+richnanney@users.noreply.github.com>
2025-07-12 17:06:14 -0600

    just checking if the rain works
    might as well

--- data/mods/watergym/rulesets.ts
+++ data/mods/watergym/rulesets.ts
@@ -3,6 +3,11 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		effectType: 'Rule',
 		name: 'WaterGym',
 		desc: "All Water-type Pokémon gain Aqua Ring at the end of each turn.",
+		onBegin() {
+			this.add('-weather', 'Rain Dance');
+			this.field.weather = 'raindance' as ID;
+			this.field.weatherState = { id: 'raindance'};
+		},
 		onResidual(battle) {
 			for (const side of this.sides) {
 				for (const pokemon of side.active) {
@@ -14,5 +19,6 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 				}
 			}
 		},
+
 	},
 };
