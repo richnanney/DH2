780eade9c209e3cc0736126c78c324d775cc9d02
Richard Nanney <79386248+richnanney@users.noreply.github.com>
2025-07-12 16:56:37 -0600

    whoopsie shouldve left those commented out

--- config/formats.ts
+++ config/formats.ts
@@ -39,7 +39,7 @@ export const Formats: FormatList = [
 		name: "[Gen 8] Water Gym",
 		mod: 'watergym',
 		ruleset: ['watergym']
-	},
+	},/*
 	{
 		name: "[Gen 8] Ghost Gym",
 		mod: 'ghostgym',
@@ -59,5 +59,5 @@ export const Formats: FormatList = [
 			{
 		name: "[Gen 8] Psychic Gym",
 		mod: 'psychicgym',
-	},
+	},*/
 ];
