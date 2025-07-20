0de89ee19bc222ada15ad4936b16e5326aaacabd
Richard Nanney <79386248+richnanney@users.noreply.github.com>
2025-07-20 16:23:00 -0600

    fire gym debug tools

--- data/mods/unboundgyms/rulesets.ts
+++ data/mods/unboundgyms/rulesets.ts
@@ -25,6 +25,16 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 			this.field.weather = 'sunnyday' as ID;
 			this.field.weatherState = { id: 'sunnyday'};
 		},
+		onAnySetWeather(target, source, weather) {
+			if (this.field.weather == 'sunnyday') {
+				this.add('-message', 'The sun from the gym effect can\'t be overwritten!')
+				if (source != null){
+					this.add('-message', `Debug: Weather source is ${source.fullname}`)
+					this.add('-message', `Debug: Weather source is ${source}`)
+
+				}
+			}
+		},
 	},
 	grassgym: {
 		effectType: 'Rule',
