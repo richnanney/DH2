6c4c3a6291feeeca3d0c52f1bc4ea551713d9d58
Richard Nanney <79386248+richnanney@users.noreply.github.com>
2026-09-07 23:31:41 -0600

    Update rulesets.ts

--- data/mods/unboundgyms/rulesets.ts
+++ data/mods/unboundgyms/rulesets.ts
@@ -441,31 +441,39 @@ export const Rulesets: import('../../../sim/dex-formats').FormatDataTable = {
 		name: 'Poison Gym',
 		desc: "Randomizes non-Poison secondary types, or assigns a random type to the secondary type.",
 		onModifySpecies(species, target, source, effect) {
+			if (this.turn > 0) return;
 			if (!target) return; // Chat command
 			if (effect && ['imposter', 'transform'].includes(effect.id)) return;
 			const allTypes = [ 'Normal','Grass','Fire','Water','Electric','Bug','Flying','Rock','Poison','Ground','Ice','Fighting','Psychic','Ghost','Dragon','Dark','Steel','Fairy']
 			const thisTypes = target.getTypes()
+			var newtypes = []
 			if (thisTypes.length > 1)
 			{
 				this.add('-message', `${target.name} has two types. Those types are ${thisTypes[0]} and ${thisTypes[1]}.`)
 				if(thisTypes[1] != 'Poison'){
 					const validTypes = allTypes.filter(item => !thisTypes[0].includes(item))
-					this.add('-message', `Here's a random type that this pokemon might get assigned to replace ${thisTypes[1]}: ${validTypes[Math.floor(Math.random() * validTypes.length)]}.`)
+					const thisNewType = validTypes[Math.floor(Math.random() * validTypes.length)]
+					this.add('-message', `Here's the new type that will replace ${thisTypes[1]}: ${thisNewType}.`)
+					newtypes = [thisTypes[0], thisNewType]
 				}
 				else{
 					const validTypes = allTypes.filter(item => !thisTypes[1].includes(item))
-					this.add('-message', `Here's a random type that this pokemon might get assigned to replace ${thisTypes[0]}: ${validTypes[Math.floor(Math.random() * validTypes.length)]}.`)
+					const thisNewType = validTypes[Math.floor(Math.random() * validTypes.length)]
+					this.add('-message', `Here's the new type that will replace ${thisTypes[0]}: ${thisNewType}.`)
+					newtypes = [thisNewType, thisTypes[1]]
 				}
 				
 			}
 			else 
 			{
 				const validTypes = allTypes.filter(item => !thisTypes[0].includes(item))
+				const thisNewType = validTypes[Math.floor(Math.random() * validTypes.length)]
 				this.add('-message', `${target.name} has one type. That type is ${thisTypes[0]}.`)
-				this.add('-message', `Here's a random type that this pokemon might get assigned as a secondary type: ${validTypes[Math.floor(Math.random() * validTypes.length)]}.`)
+				this.add('-message', `Here's the new secondary type: ${thisNewType}.`)
+				newtypes = [thisTypes[0], thisNewType]
 			}
 			//const types = [...new Set(target.baseMoveSlots.slice(0, 2).map(move => this.dex.moves.get(move.id).type))];
-			return { ...species, thisTypes};
+			return { ...species, newtypes};
 		},
 		/*
 		effectType: 'Rule',
